2e985dd09dd3c68c86ca74516cc4de4eebed3f82
odedron100 <odedron84@gmail.com>
2020-09-20 14:35:23 +0300

    Add basic chat window

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 
 class Chat extends Component {
+  state = {
+  	isChatWindowOpen: false
+  }
+
+  openChatWindow = () => {
+  	this.setState({isChatWindowOpen: !this.state.isChatWindowOpen});
+  }
+
   render() {
+  	const {isChatWindowOpen} = this.state;
+
     return (
       <div className="chat-container">
-        <div className="open-chat-button"> נציג צ׳אט זמין עבורך </div>
+        <div className="open-chat-button" onClick={this.openChatWindow}> נציג צ׳אט זמין עבורך </div>
+        {isChatWindowOpen && <div className="chat-window"></div>}
       </div>
     );
   } 
