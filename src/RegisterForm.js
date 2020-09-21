0e3e0dac8a0c041c23069be2feacb22cb07875a7
odedron100 <odedron84@gmail.com>
2020-09-21 16:38:28 +0300

    Add agent view register form

--- src/RegisterForm.js
+++ src/RegisterForm.js
@@ -4,13 +4,14 @@ class RegisterForm extends Component {
 	render() {
 		return (
 			<div className="register-form-container">
+			<div className="title">צור משתמש חדש</div>
 				<div className="form">
 					<input className="name-input" className="input" type="text" placeholder="שם פרטי" />
 					<input className="last-name-input" className="input" type="text" placeholder="שם משפחה" />
 					<input className="email-input" className="input" type="text" placeholder="כתובת מייל" />
-					<input className="password-input" className="input" type="text" placeholder="סיסמא" />
-					<input className="confirm-password-input" className="input" type="text" placeholder="אימות סיסמא" />
-
+					<input className="password-input" className="input" type="password" placeholder="סיסמא" />
+					<input className="confirm-password-input" className="input" type="password" placeholder="אימות סיסמא" />
+					<div className="create-user-button"> סיימתי, צור משתמש!</div>
 				</div>
 			</div>
 		);
