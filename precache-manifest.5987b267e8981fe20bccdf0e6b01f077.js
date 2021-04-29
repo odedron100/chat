df5d839e4d1f4eb5789d5f0ad7bfd0a014d5c070
odedron100 <odedron84@gmail.com>
2021-04-29 11:24:58 +0300

    Updates

---
+++ precache-manifest.5987b267e8981fe20bccdf0e6b01f077.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568a1131fd87a03487f15a7ba69fc280",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "75b3f0c26c779459fbda",
+    "url": "/chat/static/css/main.abad62fc.chunk.css"
+  },
+  {
+    "revision": "20f1231c9ec2f2dc023f",
+    "url": "/chat/static/js/2.658b6636.chunk.js"
+  },
+  {
+    "revision": "75fc5a0ff22eb52fcd50272201589016",
+    "url": "/chat/static/js/2.658b6636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b3f0c26c779459fbda",
+    "url": "/chat/static/js/main.0771e8c8.chunk.js"
+  },
+  {
+    "revision": "8d86be0bcc4812848edd",
+    "url": "/chat/static/js/runtime-main.6b736942.js"
+  }
+]);
